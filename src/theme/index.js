c9f6270902b839c92511cdf68e15ae6ce5e0f48d
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-05-03 23:19:36 +0600

    test primary theme functionality

---
+++ src/theme/index.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2D6A4F",
+    },
+    secondary: {
+      main: "#74C69D",
+    },
+  },
+});
+
+export default theme;
