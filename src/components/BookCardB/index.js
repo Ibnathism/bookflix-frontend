875c2c8ee1e389334adc45d712c81af90a253ac8
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-05-07 00:25:56 +0600

    card B final

--- src/components/BookCardB/index.js
+++ src/components/BookCardB/index.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,33 +21,45 @@ const useStyles = makeStyles((theme) => ({
     height: "100%",
     background: theme.palette.secondary.main,
     borderRadius: "0px 8px 8px 0px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
   },
-  genre: {
-    padding: theme.spacing(1),
+  review: {
+    padding: theme.spacing(2),
+  },
+  button: {
+    width: "100px",
+    height: "43px",
+    margin: theme.spacing(2),
+    background: theme.palette.background.default,
     display: "flex",
     alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "flex-start",
+    justifyContent: "center",
+    border: "1px solid #081C15",
+    borderRadius: theme.spacing(0.5),
   },
-  genreName: {
-    display: "flex",
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-  },
-  icons: {
-    padding: theme.spacing(0, 1, 2, 1),
-    display: "flex",
-    alignItems: "flex-start",
-    justifyContent: "space-between",
+  buttonText: {
+    color: theme.palette.info.main,
   },
 }));
-const BookCardB = ({ imageUrl, genreList }) => {
+const BookCardB = ({ imageUrl, review }) => {
   const classes = useStyles();
   return (
     <>
       <Box className={classes.root}>
         <img className={classes.leftContainer} alt="book" src={imageUrl} />
-        <div className={classes.rightContainer}></div>
+        <div className={classes.rightContainer}>
+          <Typography className={classes.review} variant="body1">
+            {review}
+          </Typography>
+          <div className={classes.button}>
+            <Typography className={classes.buttonText} variant="body1">
+              Read Later
+            </Typography>
+          </div>
+        </div>
       </Box>
     </>
   );
--- src/views/TestView/index.js
+++ src/views/TestView/index.js
@@ -1,3 +1,5 @@
+import { Container } from "@material-ui/core";
+import BookCardA from "../../components/BookCardA";
 import BookCardB from "../../components/BookCardB";
 const TestView = () => {
   const genreList = [
@@ -11,10 +13,20 @@ const TestView = () => {
       name: "Drama",
     },
   ];
+  const review =
+    "“As thrilling and smart as it is terrifying. There have been a number of big-gun literary series brought to screen over the past decade. This slays them all.”  - The Guardian";
   return (
-    <div>
-      <BookCardB imageUrl="/images/10.jpg" genreList={genreList} />
-    </div>
+    <Container
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "flex-start",
+        justifyContent: "space-around",
+      }}
+    >
+      <BookCardA imageUrl="/images/10.jpg" genreList={genreList} />
+      <BookCardB imageUrl="/images/10.jpg" review={review} />
+    </Container>
   );
 };
 
