cf2994fd32a746bd0f8b8a18ea96a52d14acdeed
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-05-12 21:59:07 +0600

    add common layout

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,29 +6,16 @@ const Navbar = () => {
     <>
       <Nav>
         <Bars />
-
         <NavMenu>
-          <NavLink to="/about" activeStyle>
-            About
+          <NavLink to="/home" activeStyle>
+            Home
           </NavLink>
-          <NavLink to="/events" activeStyle>
-            Events
-          </NavLink>
-          <NavLink to="/annual" activeStyle>
-            Annual Report
-          </NavLink>
-          <NavLink to="/team" activeStyle>
-            Teams
-          </NavLink>
-          <NavLink to="/blogs" activeStyle>
-            Blogs
-          </NavLink>
-          <NavLink to="/sign-up" activeStyle>
-            Sign Up
+          <NavLink to="/list" activeStyle>
+            My List
           </NavLink>
         </NavMenu>
         <NavBtn>
-          <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          <NavBtnLink to="/logout">Log Out</NavBtnLink>
         </NavBtn>
       </Nav>
     </>
