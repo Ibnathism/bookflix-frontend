b0c4e250b1296316304096b1236326e48ce711bc
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-05-12 22:08:03 +0600

    refactor nav style

--- src/components/Navbar/elements.js
+++ src/components/Navbar/elements.js
@@ -3,18 +3,16 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  background: #63d471;
-  height: 85px;
+  background: #081c15;
+  height: 65px;
   display: flex;
   justify-content: space-between;
   padding: 0.2rem calc((100vw - 1000px) / 2);
   z-index: 12;
-  /* Third Nav */
-  /* justify-content: flex-start; */
 `;
 
 export const NavLink = styled(Link)`
-  color: #808080;
+  color: #d8f3dc;
   display: flex;
   align-items: center;
   text-decoration: none;
@@ -22,13 +20,13 @@ export const NavLink = styled(Link)`
   height: 100%;
   cursor: pointer;
   &.active {
-    color: #000000;
+    color: #74c69d;
   }
 `;
 
 export const Bars = styled(FaBars)`
   display: none;
-  color: #808080;
+  color: #d8f3dc;
   @media screen and (max-width: 768px) {
     display: block;
     position: absolute;
@@ -44,11 +42,6 @@ export const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-right: -24px;
-  /* Second Nav */
-  /* margin-right: 24px; */
-  /* Third Nav */
-  /* width: 100vw;
-  white-space: nowrap; */
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -58,9 +51,6 @@ export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
   margin-right: 24px;
-  /* Third Nav */
-  /* justify-content: flex-end;
-  width: 100vw; */
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -68,19 +58,18 @@ export const NavBtn = styled.nav`
 
 export const NavBtnLink = styled(Link)`
   border-radius: 4px;
-  background: #808080;
+  background: #2d6a4f;
   padding: 10px 22px;
-  color: #000000;
+  color: #d8f3dc;
   outline: none;
   border: none;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
-  /* Second Nav */
   margin-left: 24px;
   &:hover {
     transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #808080;
+    background: #d8f3dc;
+    color: #2d6a4f;
   }
 `;
