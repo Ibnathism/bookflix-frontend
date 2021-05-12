e85b739b7dca865b1a48fb0f2ee86002d4be31be
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-05-12 22:31:36 +0600

    my list page

--- src/views/MyList/index.js
+++ src/views/MyList/index.js
@@ -1,6 +1,29 @@
+import { Container, Grid, Typography } from "@material-ui/core";
 import CommonLayout from "../../layouts/CommonLayout";
+import BookCardA from "../../components/BookCardA";
+import MyList from "../../data/mylist.json";
 const MyListView = () => {
-  return <CommonLayout>MyList</CommonLayout>;
+  return (
+    <CommonLayout>
+      <Container>
+        <Typography variant="h2" style={{ marginTop: "16px" }}>
+          In your list
+        </Typography>
+        <Grid container spacing={3} style={{ marginTop: "16px" }}>
+          {MyList.map((book, id) => {
+            return (
+              <Grid item key={id}>
+                <BookCardA
+                  imageUrl={book.imageUrl}
+                  genreList={book.genreList}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </CommonLayout>
+  );
 };
 
 export default MyListView;
