26de473233cc0778c5e3f1862df780d325378af5
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-06-18 23:28:00 +0600

    test basic weather filter using graphql

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import HomeView from "./views/Home";
 import LoginView from "./views/Login";
 import SignupView from "./views/Signup";
 import MyListView from "./views/MyList";
-//import TestView from "./views/TestView";
+import TestView from "./views/TestView";
 import BookReaderView from "./views/BookReader";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://graphql-weather-api.herokuapp.com/",
+  });
   return (
-    <Router>
-      <Route path="/" exact component={LoginView} />
-      <Route path="/login" exact component={LoginView} />
-      <Route path="/signup" exact component={SignupView} />
-      <Route path="/home" exact component={HomeView} />
-      <Route path="/home/:id" exact component={DetailsView} />
-      <Route path="/list" exact component={MyListView} />
-      <Route path="/read" exact component={BookReaderView} />
-      <GlobalStyles />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Route path="/" exact component={LoginView} />
+        <Route path="/login" exact component={LoginView} />
+        <Route path="/signup" exact component={SignupView} />
+        <Route path="/home" exact component={HomeView} />
+        <Route path="/home/:id" exact component={DetailsView} />
+        <Route path="/list" exact component={MyListView} />
+        <Route path="/read" exact component={BookReaderView} />
+        <Route path="/test" exact component={TestView} />
+        <GlobalStyles />
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- src/views/TestView/index.js
+++ src/views/TestView/index.js
@@ -1,33 +1,64 @@
-import { Container } from "@material-ui/core";
-import BookCardA from "../../components/BookCardA";
-import BookCardB from "../../components/BookCardB";
+import { Container, Typography } from "@material-ui/core";
 import CommonLayout from "../../layouts/CommonLayout";
+import { useLazyQuery } from "@apollo/client";
+import { GET_WEATHER_QUERY } from "../../graphql/Queries";
+import { useState } from "react";
 const TestView = () => {
-  const genreList = [
+  const [city, setCity] = useState("");
+  const [details, setDetails] = useState({});
+  const [getWeather, { loading, data, error }] = useLazyQuery(
+    GET_WEATHER_QUERY,
     {
-      name: "Crime",
-    },
-    {
-      name: "Thriller",
-    },
-    {
-      name: "Drama",
-    },
-  ];
-  const review =
-    "“As thrilling and smart as it is terrifying. There have been a number of big-gun literary series brought to screen over the past decade. This slays them all.”  - The Guardian";
+      variables: {
+        name: city,
+      },
+    }
+  );
+  if (error) return <h1>Error Found</h1>;
+
+  if (data) {
+    console.log(data);
+  }
   return (
     <CommonLayout>
       <Container
         style={{
+          marginTop: "16px",
           display: "flex",
           flexDirection: "row",
           alignItems: "flex-start",
           justifyContent: "space-around",
         }}
       >
-        <BookCardA imageUrl="/images/10.jpg" genreList={genreList} />
-        <BookCardB imageUrl="/images/10.jpg" review={review} />
+        <div className="home">
+          <Typography color="primary" variant="h1">
+            Search
+          </Typography>
+          <input
+            type="text"
+            placeholder="City name..."
+            onChange={(event) => {
+              setCity(event.target.value);
+            }}
+          />
+          <button onClick={() => getWeather()}>Search</button>
+          <div className="weather">
+            {data && (
+              <>
+                <Typography variant="h1">{data.getCityByName.name}</Typography>
+                <Typography variant="h1">
+                  Temperature: {data.getCityByName.weather.temperature.actual}
+                </Typography>
+                <Typography variant="h1">
+                  Description: {data.getCityByName.weather.summary.description}
+                </Typography>
+                <Typography variant="h1">
+                  Wind Speed: {data.getCityByName.weather.wind.speed}
+                </Typography>
+              </>
+            )}
+          </div>
+        </div>
       </Container>
     </CommonLayout>
   );
