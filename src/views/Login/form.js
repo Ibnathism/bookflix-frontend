9f9b12ec4d671ea10e05d078b5df8296ace8a187
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-06-25 17:10:56 +0600

    complete login

--- src/views/Login/form.js
+++ src/views/Login/form.js
@@ -1,8 +1,9 @@
 import { Box, TextField, Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useHistory } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { LOGIN } from "../../graphql/Queries";
+import { Alert, AlertTitle } from "@material-ui/lab";
 import { useState } from "react";
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,29 +40,45 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 const LoginForm = () => {
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
+  const history = useHistory();
   const classes = useStyles();
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+
   const [login, { data, error }] = useMutation(LOGIN, {
     variables: {
       username: username,
       password: password,
     },
+    onCompleted: () => {
+      history.push("/home");
+    },
+    onError: () => {
+      setShowErrorAlert(true);
+    },
   });
 
-  const handleLogin = async () => {
-    await login();
+  const handleLogin = () => {
+    login();
+    //console.log(data);
     if (!error && data && data.login.token) {
       const token = data.login.token;
       localStorage.setItem("token", token);
-      console.log(token);
-    } else {
-      return <h1>Error</h1>;
+      //console.log(token);
     }
   };
   return (
     <>
       <Box className={classes.root}>
+        {showErrorAlert ? (
+          <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            Login Failed -- <strong>Invalid Credentials</strong>
+          </Alert>
+        ) : (
+          <></>
+        )}
         <form className={classes.form}>
           <TextField
             variant="filled"
@@ -78,16 +95,14 @@ const LoginForm = () => {
             label="Password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <RouterLink to="/home">
-            <Button
-              onClick={handleLogin}
-              variant="contained"
-              color="primary"
-              style={{ margin: "16px" }}
-            >
-              Login
-            </Button>
-          </RouterLink>
+          <Button
+            onClick={handleLogin}
+            variant="contained"
+            color="primary"
+            style={{ margin: "16px" }}
+          >
+            Login
+          </Button>
         </form>
         <div className={classes.signupText}>
           <Typography variant="h3">New on BookFlix?</Typography>
