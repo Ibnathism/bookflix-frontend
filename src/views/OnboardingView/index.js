88b20b60edb5e1eb3e4222956a010d75709afc1d
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-06-27 00:59:33 +0600

    set onboarding page to page transition

--- src/views/OnboardingView/index.js
+++ src/views/OnboardingView/index.js
@@ -1,9 +1,11 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Grid, Typography, Box } from "@material-ui/core";
+import { Container, Grid, Typography, Box, Button } from "@material-ui/core";
 import BooksOnboard from "./books";
 import GenresOnboard from "./genres";
 import AuthorsOnboard from "./authors";
 import GlobalLayout from "../../layouts/GlobalLayout";
+import { useState } from "react";
+import { useHistory } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   box: {
@@ -13,31 +15,105 @@ const useStyles = makeStyles((theme) => ({
 
 const OnboardingView = () => {
   const classes = useStyles();
+  const history = useHistory();
+  const [step, setStep] = useState(0);
   return (
     <GlobalLayout>
       <Container>
         <Box className={classes.box}>
           <Grid
             container
-            spacing={5}
+            spacing={6}
             direction="column"
             justify="center"
             alignItems="center"
           >
-            <Grid item>
-              <Typography variant="h2">
-                Select at least 5 books you like !
-              </Typography>
-            </Grid>
-            <Grid item>
-              <GenresOnboard />
-            </Grid>
-            <Grid item>
-              <AuthorsOnboard />
-            </Grid>
-            <Grid item>
-              <BooksOnboard />
-            </Grid>
+            {step === 0 ? (
+              <>
+                <Grid item>
+                  <Typography variant="h2" style={{ marginTop: "16px" }}>
+                    Select at least 3 Genres you like !
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <GenresOnboard />
+                </Grid>
+                <Grid>
+                  <Button
+                    style={{
+                      marginTop: "16px",
+                      width: "180px",
+                      height: "70px",
+                    }}
+                    variant="contained"
+                    color="primary"
+                    onClick={() => setStep(1)} //check api call validity
+                  >
+                    Continue
+                  </Button>
+                </Grid>
+              </>
+            ) : (
+              <></>
+            )}
+
+            {step === 1 ? (
+              <>
+                <Grid item>
+                  <Typography variant="h2" style={{ marginTop: "16px" }}>
+                    Select at least 3 Authors you like !
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <AuthorsOnboard />
+                </Grid>
+                <Grid>
+                  <Button
+                    style={{
+                      marginTop: "16px",
+                      width: "180px",
+                      height: "70px",
+                    }}
+                    variant="contained"
+                    color="primary"
+                    onClick={() => setStep(2)} //check api call validity
+                  >
+                    Continue
+                  </Button>
+                </Grid>
+              </>
+            ) : (
+              <></>
+            )}
+
+            {step === 2 ? (
+              <>
+                <Grid item>
+                  <Typography variant="h2" style={{ marginTop: "16px" }}>
+                    Select at least 3 Books you like !
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <BooksOnboard />
+                </Grid>
+                <Grid>
+                  <Button
+                    style={{
+                      marginTop: "16px",
+                      width: "180px",
+                      height: "70px",
+                    }}
+                    variant="contained"
+                    color="primary"
+                    onClick={() => history.push("/home")} //check api call validity
+                  >
+                    Continue
+                  </Button>
+                </Grid>
+              </>
+            ) : (
+              <></>
+            )}
           </Grid>
         </Box>
       </Container>
