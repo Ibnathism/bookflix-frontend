33b13c87f793e3bafb64888bfc542d0ec866693d
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-06-29 01:53:01 +0600

    add feed loading animation

--- src/views/BookDetails/index.js
+++ src/views/BookDetails/index.js
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 import { GET_FEED, GET_BOOK_DETAILS } from "../../graphql/Queries";
 import { useLazyQuery } from "@apollo/client";
 import { useParams, useHistory } from "react-router";
+import LottieAnimation from "../../helpers/lottie";
+import LoadAnimation from "../../animations/feed-loading.json";
 
 const DetailsView = () => {
   const [feed, setFeed] = useState([]);
@@ -39,7 +41,7 @@ const DetailsView = () => {
     getFeed();
   }, [getFeed]);
 
-  const [getBookDetails, { data: bookDetails, error: bookError }] =
+  const [getBookDetails, { data: bookDetails, loading, error: bookError }] =
     useLazyQuery(GET_BOOK_DETAILS, {
       variables: {
         id: id,
@@ -61,92 +63,98 @@ const DetailsView = () => {
 
   return (
     <CommonLayout>
-      <Grid
-        container
-        direction="column"
-        spacing={3}
-        style={{ marginTop: "16px" }}
-      >
-        <Grid item md={12} xs={12}>
-          <Container>
-            <Grid container spacing={3} alignItems="center" justify="center">
-              <Grid item xs={12} md={6} lg={6} xl={6}>
-                <img
-                  width="420px"
-                  style={{ borderRadius: "8px" }}
-                  src={`https://bookflix-dev.s3.ap-southeast-1.amazonaws.com/${details.coverImageUrl}`}
-                  alt={details.title}
-                />
-              </Grid>
-              <Grid item xs={12} md={6} lg={6} xl={6}>
-                <Grid container direction="column" spacing={6}>
-                  <Grid item>
-                    <Typography variant="h1" align="left">
-                      {details.title}
-                    </Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h2" align="left">
-                      By{" "}
-                      {details.authors.length !== 0
-                        ? details.authors[0].name
-                        : ""}
-                    </Typography>
-                  </Grid>
-                  <Grid item>
-                    <Grid
-                      container
-                      spacing={3}
-                      alignItems="center"
-                      justify="center"
-                    >
-                      {details.genres.map((genre, id) => {
-                        return (
-                          <Grid item key={id}>
-                            <Chip
-                              variant="default"
-                              color="secondary"
-                              label={genre.name}
-                            />
-                          </Grid>
-                        );
-                      })}
-                    </Grid>
-                  </Grid>
-                  <Grid item>
-                    <Grid container spacing={3}>
-                      <Typography variant="h3" align="left">
-                        {details.description}
+      {loading ? (
+        <Container>
+          <LottieAnimation lotti={LoadAnimation} height={500} width={500} />
+        </Container>
+      ) : (
+        <Grid
+          container
+          direction="column"
+          spacing={3}
+          style={{ marginTop: "16px" }}
+        >
+          <Grid item md={12} xs={12}>
+            <Container>
+              <Grid container spacing={3} alignItems="center" justify="center">
+                <Grid item xs={12} md={6} lg={6} xl={6}>
+                  <img
+                    width="420px"
+                    style={{ borderRadius: "8px" }}
+                    src={`https://bookflix-dev.s3.ap-southeast-1.amazonaws.com/${details.coverImageUrl}`}
+                    alt={details.title}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6} lg={6} xl={6}>
+                  <Grid container direction="column" spacing={6}>
+                    <Grid item>
+                      <Typography variant="h1" align="left">
+                        {details.title}
                       </Typography>
                     </Grid>
-                  </Grid>
-                  <Grid
-                    item
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                    }}
-                  >
-                    {/* <RouterLink to="/read"> */}
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => history.push(`/home/${details.id}/read`)}
+                    <Grid item>
+                      <Typography variant="h2" align="left">
+                        By{" "}
+                        {details.authors.length !== 0
+                          ? details.authors[0].name
+                          : ""}
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Grid
+                        container
+                        spacing={3}
+                        alignItems="center"
+                        justify="center"
+                      >
+                        {details.genres.map((genre, id) => {
+                          return (
+                            <Grid item key={id}>
+                              <Chip
+                                variant="default"
+                                color="secondary"
+                                label={genre.name}
+                              />
+                            </Grid>
+                          );
+                        })}
+                      </Grid>
+                    </Grid>
+                    <Grid item>
+                      <Grid container spacing={3}>
+                        <Typography variant="h3" align="left">
+                          {details.description}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                    <Grid
+                      item
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                      }}
                     >
-                      Start Reading
-                    </Button>
-                    {/* </RouterLink> */}
+                      {/* <RouterLink to="/read"> */}
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => history.push(`/home/${details.id}/read`)}
+                      >
+                        Start Reading
+                      </Button>
+                      {/* </RouterLink> */}
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          </Container>
-        </Grid>
+            </Container>
+          </Grid>
 
-        <Grid item md={12} xs={12}>
-          <Feed feed={feed} />
+          <Grid item md={12} xs={12}>
+            <Feed feed={feed} />
+          </Grid>
         </Grid>
-      </Grid>
+      )}
     </CommonLayout>
   );
 };
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -7,6 +7,8 @@ import { useState, useEffect } from "react";
 import { GET_FEED } from "../../graphql/Queries";
 import { useLazyQuery } from "@apollo/client";
 import { useHistory } from "react-router";
+import LottieAnimation from "../../helpers/lottie";
+import LoadAnimation from "../../animations/feed-loading.json";
 
 const HomeView = () => {
   const history = useHistory();
@@ -15,7 +17,7 @@ const HomeView = () => {
     books: [],
   });
 
-  const [getFeed, { data, error }] = useLazyQuery(GET_FEED, {
+  const [getFeed, { data, loading, error }] = useLazyQuery(GET_FEED, {
     variables: {
       bookCountEachCategory: 10,
       categoryCount: 10,
@@ -37,90 +39,96 @@ const HomeView = () => {
 
   return (
     <CommonLayout>
-      <Grid container direction="column">
-        <Grid item>
-          <Container>
-            <Box style={{ margin: "16px" }}>
-              <Grid
-                container
-                direction="column"
-                justify="center"
-                alignItems="center"
-              >
-                <Grid item>
-                  <Typography variant="h1">Top Chart</Typography>
-                </Grid>
-                <Grid item>
-                  <Grid
-                    container
-                    spacing={3}
-                    direction="row"
-                    alignItems="center"
-                    justify="center"
-                    style={{ marginTop: "16px" }}
-                  >
-                    {topPicks.books.slice(0, 3).map((book, id) => {
-                      var width = id === 1 ? "100%" : "80%";
-                      return (
-                        <Grid
-                          item
-                          key={id}
-                          xs={12}
-                          md={4}
-                          lg={4}
-                          style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                          }}
-                        >
+      {loading ? (
+        <Container>
+          <LottieAnimation lotti={LoadAnimation} height={500} width={500} />
+        </Container>
+      ) : (
+        <Grid container direction="column">
+          <Grid item>
+            <Container>
+              <Box style={{ margin: "16px" }}>
+                <Grid
+                  container
+                  direction="column"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Grid item>
+                    <Typography variant="h1">Top Chart</Typography>
+                  </Grid>
+                  <Grid item>
+                    <Grid
+                      container
+                      spacing={3}
+                      direction="row"
+                      alignItems="center"
+                      justify="center"
+                      style={{ marginTop: "16px" }}
+                    >
+                      {topPicks.books.slice(0, 3).map((book, id) => {
+                        var width = id === 1 ? "100%" : "80%";
+                        return (
                           <Grid
-                            container
-                            direction="column"
-                            alignItems="center"
-                            justify="center"
-                            spacing={3}
+                            item
+                            key={id}
+                            xs={12}
+                            md={4}
+                            lg={4}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
                           >
-                            {
-                              //TODO: Need rank for books from backend
-                            }
-                            <Typography variant="h1">
-                              # {id === 0 ? 2 : id === 2 ? 3 : 1}
-                            </Typography>
-                            <img
-                              width={width}
-                              src={`https://bookflix-dev.s3.ap-southeast-1.amazonaws.com/${book.coverImageUrl}`}
-                              alt={book.title}
-                            />
-                            {/* <RouterLink to="/home/1"> */}
-                            <Button
-                              type="submit"
-                              variant="contained"
-                              color="primary"
-                              onClick={() => history.push(`/home/${book.id}`)}
-                              style={{
-                                margin: "16px",
-                                height: "40px",
-                                width: "100px",
-                              }}
+                            <Grid
+                              container
+                              direction="column"
+                              alignItems="center"
+                              justify="center"
+                              spacing={3}
                             >
-                              Read
-                            </Button>
-                            {/* </RouterLink> */}
+                              {
+                                //TODO: Need rank for books from backend
+                              }
+                              <Typography variant="h1">
+                                # {id === 0 ? 2 : id === 2 ? 3 : 1}
+                              </Typography>
+                              <img
+                                width={width}
+                                src={`https://bookflix-dev.s3.ap-southeast-1.amazonaws.com/${book.coverImageUrl}`}
+                                alt={book.title}
+                              />
+                              {/* <RouterLink to="/home/1"> */}
+                              <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                                onClick={() => history.push(`/home/${book.id}`)}
+                                style={{
+                                  margin: "16px",
+                                  height: "40px",
+                                  width: "100px",
+                                }}
+                              >
+                                Read
+                              </Button>
+                              {/* </RouterLink> */}
+                            </Grid>
                           </Grid>
-                        </Grid>
-                      );
-                    })}
+                        );
+                      })}
+                    </Grid>
                   </Grid>
                 </Grid>
-              </Grid>
-            </Box>
-          </Container>
+              </Box>
+            </Container>
+          </Grid>
+          <Grid item>
+            <Feed feed={feed} />
+          </Grid>
         </Grid>
-        <Grid item>
-          <Feed feed={feed} />
-        </Grid>
-      </Grid>
+      )}
     </CommonLayout>
   );
 };
