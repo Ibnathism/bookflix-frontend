eefcd8df7d417b12dfb241375b5bbaa57dfa0552
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-07-16 14:18:51 +0600

    refactor navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,132 @@
 import React from "react";
-import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from "./elements";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+} from "@material-ui/core";
+import { alpha, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import ExitToApp from "@material-ui/icons/ExitToApp";
+import { NavLink } from "./elements";
 
-const Navbar = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  exitButton: {
+    marginLeft: theme.spacing(2),
+  },
+  left: {
+    flexGrow: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
+  title: {
+    marginRight: theme.spacing(8),
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "24ch",
+      "&:focus": {
+        width: "32ch",
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+
   return (
-    <>
-      <Nav>
-        <Bars />
-        <NavMenu>
-          <NavLink to="/home">Home</NavLink>
-          <NavLink to="/list">My List</NavLink>
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to="/login">Log Out</NavBtnLink>
-        </NavBtn>
-      </Nav>
-    </>
-  );
-};
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <div className={classes.left}>
+            <Typography className={classes.title} variant="h2" noWrap>
+              BookFlix
+            </Typography>
 
-export default Navbar;
+            <Typography variant="h6" noWrap>
+              <NavLink to="/home">Home</NavLink>
+            </Typography>
+
+            <Typography variant="h6" noWrap>
+              <NavLink to="/list">My List</NavLink>
+            </Typography>
+          </div>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <IconButton
+            edge="start"
+            className={classes.exitButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <NavLink to="/login">
+              <ExitToApp />
+            </NavLink>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
