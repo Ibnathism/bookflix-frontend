f92e41f6ccb151d8161a08bf8af969db6eccacf5
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2021-07-16 21:28:05 +0600

    refactor

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,9 @@
-import React, { useState, useEffect } from "react";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Grid,
-} from "@material-ui/core";
+import React from "react";
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import ExitToApp from "@material-ui/icons/ExitToApp";
 import { NavLink } from "./elements";
-
-import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import { useHistory } from "react-router";
-import { SEARCH } from "../../graphql/Queries";
-import { useLazyQuery } from "@apollo/client";
+import Search from "./search";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -81,29 +69,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const SearchAppBar = () => {
-  const history = useHistory();
-  const [open, setOpen] = useState(false);
-  const [options, setOptions] = useState([]);
-  const [searchText, setSearchText] = useState();
   const classes = useStyles();
-
-  const [search, { data, loading }] = useLazyQuery(SEARCH, {
-    variables: {
-      filter: searchText,
-    },
-    onCompleted: () => {
-      console.log("on completed of search", data.search);
-      const res = JSON.parse(JSON.stringify(data.search));
-      setOptions(res.books);
-    },
-  });
-
-  useEffect(() => {
-    if (searchText?.length >= 3) {
-      search();
-    }
-  }, [searchText, search]);
-
   return (
     <div className={classes.root}>
       <AppBar position="static" color="transparent">
@@ -112,90 +78,15 @@ const SearchAppBar = () => {
             <Typography className={classes.title} variant="h2" noWrap>
               BookFlix
             </Typography>
-
             <Typography variant="h6" noWrap>
               <NavLink to="/home">Home</NavLink>
             </Typography>
-
             <Typography variant="h6" noWrap>
               <NavLink to="/list">My List</NavLink>
             </Typography>
           </div>
           <div className={classes.search}>
-            {/* <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div> */}
-            <Autocomplete
-              id="asynchronous-demo"
-              style={{ width: 300 }}
-              open={open}
-              onOpen={() => {
-                setOpen(true);
-              }}
-              onClose={() => {
-                setOpen(false);
-                //history.push(`/home/1`);
-              }}
-              getOptionSelected={(option, value) =>
-                option.title === value.title
-              }
-              onChange={(event) => {
-                console.log("in the on change", event.target.textContent);
-                if (event.target.textContent !== "") {
-                  let books = options.filter(
-                    (item) => item.title === event.target.textContent
-                  );
-                  console.log(books);
-                  books.length !== 0
-                    ? history.push(`/home/${books[0].id}`)
-                    : console.log("Book not found");
-                }
-              }}
-              getOptionLabel={(option) => option.title}
-              options={options}
-              loading={loading}
-              renderOption={(option) => (
-                <React.Fragment>
-                  <img
-                    alt="img"
-                    width="50px"
-                    height="70px"
-                    src={`https://bookflix-dev.s3.ap-southeast-1.amazonaws.com/${option.coverImageUrl}`}
-                  />
-                  <Grid container style={{ padding: "8px" }}>
-                    <Grid>
-                      <Typography variant="body1">{option.title}</Typography>
-                    </Grid>
-                    <Grid>
-                      <Typography variant="body1">
-                        {option.authors.length !== 0
-                          ? option.authors[0].name
-                          : ""}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </React.Fragment>
-              )}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant="outlined"
-                  value={searchText}
-                  onChange={(event) => setSearchText(event.target.value)}
-                  InputProps={{
-                    ...params.InputProps,
-                    endAdornment: (
-                      <React.Fragment>
-                        {loading ? (
-                          <CircularProgress color="inherit" size={20} />
-                        ) : null}
-                        {params.InputProps.endAdornment}
-                      </React.Fragment>
-                    ),
-                  }}
-                />
-              )}
-            />
+            <Search />
           </div>
           <IconButton
             edge="start"
---
+++ src/components/Navbar/search.js
@@ -0,0 +1,111 @@
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import React, { useState, useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ExitToApp from "@material-ui/icons/ExitToApp";
+import { NavLink } from "./elements";
+import { useHistory } from "react-router";
+import { SEARCH } from "../../graphql/Queries";
+import { useLazyQuery } from "@apollo/client";
+const Search = () => {
+  const history = useHistory();
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [searchText, setSearchText] = useState();
+
+  const [search, { data, loading }] = useLazyQuery(SEARCH, {
+    variables: {
+      filter: searchText,
+    },
+    onCompleted: () => {
+      console.log("on completed of search", data.search);
+      const res = JSON.parse(JSON.stringify(data.search));
+      setOptions(res.books);
+    },
+  });
+
+  useEffect(() => {
+    if (searchText?.length >= 3) {
+      search();
+    }
+  }, [searchText, search]);
+  return (
+    <Autocomplete
+      id="asynchronous-demo"
+      style={{ width: 300 }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+        //history.push(`/home/1`);
+      }}
+      getOptionSelected={(option, value) => option.title === value.title}
+      onChange={(event) => {
+        console.log("in the on change", event.target.textContent);
+        if (event.target.textContent !== "") {
+          let books = options.filter(
+            (item) => item.title === event.target.textContent
+          );
+          console.log(books);
+          books.length !== 0
+            ? history.push(`/home/${books[0].id}`)
+            : console.log("Book not found");
+        }
+      }}
+      getOptionLabel={(option) => option.title}
+      options={options}
+      loading={loading}
+      renderOption={(option) => (
+        <React.Fragment>
+          <img
+            alt="img"
+            width="50px"
+            height="70px"
+            src={`https://bookflix-dev.s3.ap-southeast-1.amazonaws.com/${option.coverImageUrl}`}
+          />
+          <Grid container style={{ padding: "8px" }}>
+            <Grid>
+              <Typography variant="body1">{option.title}</Typography>
+            </Grid>
+            <Grid>
+              <Typography variant="body1">
+                {option.authors.length !== 0 ? option.authors[0].name : ""}
+              </Typography>
+            </Grid>
+          </Grid>
+        </React.Fragment>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="outlined"
+          value={searchText}
+          onChange={(event) => setSearchText(event.target.value)}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export default Search;
